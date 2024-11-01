774b083a4f978db2fcecd67d068d763704c4012c
prrodriguess <pr.rodriguessrp@gmail.com>
2024-11-01 17:56:29 -0300

    ajustes para enviar video

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const ffmpeg = createFFmpeg({log: true});
 function App() {
 
   const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
 
   const load = async () => {
     await ffmpeg.load();
@@ -17,12 +18,20 @@ function App() {
     load();
   }, [])
 
-  return ready ? (
+  return (
     <div className="App">
+    { video && <video
+      controls
+      width="250"
+      src={URL.createObjectURL(video)}>
+
+    </video>}
+
+
+    <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
 
     </div>
-  ) : 
-  (<p>Loading...</p>);
+  )
 }
 
 export default App;
